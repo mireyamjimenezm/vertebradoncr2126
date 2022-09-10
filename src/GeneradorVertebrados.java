291bdd77a509506be6ce953d7629e3c4bcf8003c
mireyamjimenezm <mireyamjimenezm>
2022-09-09 19:56:42 -0500

    Prueba

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,7 +1,7 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
+        miVertebrados[0] = new Vertebrado("Camell-o", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
         
